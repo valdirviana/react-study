96b2e56d3b09eee4e3222353cc2ec4e326af1123
Valdir Lopes Viana <valdir.viana@hbsis.com.br>
2017-07-25 22:54:50 -0300

    update files and study in codeacademy react course

---
+++ my-app/src/game/square.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Square extends Component {
+  render() {
+    return (
+      <button className="squere">
+        {/* */}
+      </button>
+    );
+  }
+}
+
+export default Square;
